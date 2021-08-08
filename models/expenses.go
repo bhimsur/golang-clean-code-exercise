a3b9bd5c93794dce260ad6790dc5c1ee50d30d68
bhimantoro <bhimantoros@gmail.com>
2021-08-08 09:30:18 +0700

    refactor sales n expenses

--- models/expenses.go
+++ models/expenses.go
@@ -6,16 +6,5 @@ type expenses struct {
 
 // TODO: instantiate from transaction model
 func NewExpenses(d []int) Transactions {
-        data := make(map[int]int)
-
-        for i, v := range d {
-                data[i] = v
-        }
-
-        // i.e. return NewTransactions(d)
-        return &expenses{data: data}
-}
-
-func (e *expenses) Get(idx int) int {
-        return e.data[idx]
+        return NewTransactions(d)
 }
--- models/sales.go
+++ models/sales.go
@@ -6,16 +6,5 @@ type sales struct {
 
 // TODO: instantiate from transaction model
 func NewSales(d []int) Transactions {
-        data := make(map[int]int)
-
-        for i, v := range d {
-                data[i] = v
-        }
-
-        // i.e. return NewTransactions(d)
-        return &sales{data: data}
-}
-
-func (s *sales) Get(idx int) int {
-        return s.data[idx]
+        return NewTransactions(d)
 }
