618c9dd34863db1787c6c853c4534e37a788b72b
bhimantoro <bhimantoros@gmail.com>
2021-08-08 09:35:21 +0700

    refactor expenses and sales type

--- models/expenses.go
+++ models/expenses.go
@@ -1,9 +1,5 @@
 package models
 
-type expenses struct {
-        data map[int]int
-}
-
 // TODO: instantiate from transaction model
 func NewExpenses(d []int) Transactions {
         return NewTransactions(d)
--- models/sales.go
+++ models/sales.go
@@ -1,9 +1,5 @@
 package models
 
-type sales struct {
-        data map[int]int
-}
-
 // TODO: instantiate from transaction model
 func NewSales(d []int) Transactions {
         return NewTransactions(d)
